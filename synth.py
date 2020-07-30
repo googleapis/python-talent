2ab7acd1fa2098717f5e8a3fa5c0413cc4a3c8de
Xiaozhen Liu <xiaozhenliu@google.com>
2020-07-30 05:31:15 +0000

    edit synthtool script

--- synth.py
+++ synth.py
@@ -50,8 +50,11 @@ s.replace(
 # ----------------------------------------------------------------------------
 # Add templated files
 # ----------------------------------------------------------------------------
-templated_files = common.py_library(cov_level=83)
-s.move(templated_files)
+templated_files = common.py_library(
+    samples=False,  # set to True only if there are samples
+    microgenerator=True,
+)
+s.move(templated_files, excludes=[".coveragerc"])  # microgenerator has a good .coveragerc file
 
 # TODO(busunkim): Use latest sphinx after microgenerator transition
 s.replace("noxfile.py", """['"]sphinx['"]""", '"sphinx<3.0.0"')
