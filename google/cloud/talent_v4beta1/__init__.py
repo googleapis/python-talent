8540c8f0f08fcadff606676bd170b2356b002974
yoshi-automation <yoshi-automation@google.com>
2020-12-23 09:38:03 -0800

    feat: create the client for artifactregistry API.
    PiperOrigin-RevId: 338294847
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Wed Oct 21 10:47:20 2020 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: 0f626de4544db840d04b9cd777dfb1197702244a
    Source-Link: https://github.com/googleapis/googleapis/commit/0f626de4544db840d04b9cd777dfb1197702244a

--- google/cloud/talent_v4beta1/__init__.py
+++ google/cloud/talent_v4beta1/__init__.py
@@ -186,7 +186,6 @@ __all__ = (
     "EmployerFilter",
     "EmploymentRecord",
     "EmploymentType",
-    "EventServiceClient",
     "GetApplicationRequest",
     "GetCompanyRequest",
     "GetJobRequest",
@@ -244,6 +243,7 @@ __all__ = (
     "SpellingCorrection",
     "SummarizedProfile",
     "Tenant",
+    "TenantServiceClient",
     "TimeFilter",
     "TimestampRange",
     "UpdateApplicationRequest",
@@ -253,5 +253,5 @@ __all__ = (
     "UpdateTenantRequest",
     "Visibility",
     "WorkExperienceFilter",
-    "TenantServiceClient",
+    "EventServiceClient",
 )
